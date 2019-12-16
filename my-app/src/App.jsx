df3cccefaf22e91c3d3c325a8c85f084c2fc1903
Rodrigo <rodrigo.mirapalheta@pipedrive.com>
2019-12-17 01:49:57 +0200

    Countdown Watch app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
+
+import {Form, FormControl, Button, Jumbotron, Spinner} from 'react-bootstrap';
+import './App.css'
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2019',
+            newDeadline:''
+        }
+    }
+
+    changeDeadline() {
+        this.setState({deadline:this.state.newDeadline});
+    }
+
     render() {
         return(
-            <div>
-                <div>Countdown to December 25, 2019</div>
-                <div>
-                    <div>14 Days</div>
-                    <div>30 Hours</div>
-                    <div>15 Minutes</div>
-                    <div>20 Seconds</div>
-                </div>
-                <div>
-                    <input placeholder='new date'/>
-                    <button> Submit </button>
-                </div>
-                
-            </div>
+            <Jumbotron className='App'>
+                <div className='App-title'>Countdown to {this.state.deadline}</div>
+                <Clock 
+                deadline={this.state.deadline}
+                />
+                <Form >
+                    <FormControl 
+                    type="text"
+                    className='Deadline-input'
+                    placeholder='new date'
+                    onChange={event => this.setState({newDeadline:event.target.value})}
+                    />
+                    <Button 
+                    variant="outline-primary"
+                    onClick={() => this.changeDeadline()}
+                    > 
+                    Submit 
+                    </Button>
+                </Form>
+                <Spinner animation="border" />
+            </Jumbotron>
         )
     }
 }
